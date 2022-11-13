2aeea7d1859ae47330217fb4617ca22ee3e9b54f
beniko19 <martin192012@gmail.com>
2022-11-13 19:51:25 -0300

    created the data class to hold the questions data

---
+++ app/src/main/java/ar/beniko/myquizapp/Question.kt
@@ -0,0 +1,11 @@
+package ar.beniko.myquizapp
+
+data class Question(
+    val id: Int,
+    val questions: String,
+    val image: Int,
+    val optionOne: String,
+    val optionTwo: String,
+    val optionThree: String,
+    val optionFour: String,
+    val correctAnswer: Int)

9c4a9166926bbd1ca2ce2155f39d38f7dbf8994d
beniko19 <martin192012@gmail.com>
2022-11-14 15:16:49 -0300

    adding the flags and questions with each country

---
+++ app/src/main/java/ar/beniko/myquizapp/Constants.kt
@@ -0,0 +1,82 @@
+package ar.beniko.myquizapp
+
+object Constants {
+
+    fun getQuestions(question: String): ArrayList<Question> = createQuestions(question)
+
+    private fun createQuestions(question: String): ArrayList<Question> {
+        val questions = ArrayList<Question>()
+
+        val que1 = Question(
+            1, question, R.drawable.ic_flag_of_argentina,
+            "Argentina", "Australia",
+            "Armenia", "Austria", 1
+        )
+        questions.add(que1)
+
+        val que2 = Question(
+            2, question, R.drawable.ic_flag_of_australia,
+            "Brazil", "Australia",
+            "Fiji", "Austria", 2
+        )
+        questions.add(que2)
+
+        val que3 = Question(
+            3, question,
+            R.drawable.ic_flag_of_belgium, "India",
+            "New Zealand", "Belgium", "Kuwat", 3
+        )
+        questions.add(que3)
+
+        val que4 = Question(
+            4, question, R.drawable.ic_flag_of_brazil,
+            "Argentina", "Germany",
+            "New Zealand", "Brazil", 4
+        )
+        questions.add(que4)
+
+        val que5 = Question(
+            5, question, R.drawable.ic_flag_of_denmark,
+            "Denmark", "Germany",
+            "Fiji", "Australia", 1
+        )
+        questions.add(que5)
+
+        val que6 = Question(
+            6, question, R.drawable.ic_flag_of_fiji,
+            "Chile", "Peru",
+            "Fiji", "Australia", 3
+        )
+        questions.add(que6)
+
+        val que7 = Question(
+            7, question, R.drawable.ic_flag_of_germany,
+            "Russia", "Peru",
+            "Germany", "Spain", 3
+        )
+        questions.add(que7)
+
+        val que8 = Question(
+            8, question, R.drawable.ic_flag_of_india,
+            "India", "United States",
+            "Denmark", "Paraguay", 1
+        )
+        questions.add(que8)
+
+        val que9 = Question(
+            9, question, R.drawable.ic_flag_of_kuwait,
+            "India", "Belgium",
+            "Germany", "Kuwait", 4
+        )
+        questions.add(que9)
+
+        val que10 = Question(
+            10, question, R.drawable.ic_flag_of_new_zealand,
+            "Russia", "New Zealand",
+            "Brazil", "Argentina", 2
+        )
+        questions.add(que10)
+
+        return questions
+    }
+}
--- app/src/main/java/ar/beniko/myquizapp/Question.kt
+++ app/src/main/java/ar/beniko/myquizapp/Question.kt
@@ -2,7 +2,7 @@ package ar.beniko.myquizapp
 
 data class Question(
     val id: Int,
-    val questions: String,
+    val question: String,
     val image: Int,
     val optionOne: String,
     val optionTwo: String,
